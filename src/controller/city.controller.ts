27b40e8249de176b5eb0a26a66d602f3f120b867
danial <dpezeshpoor@yahoo.com>
2022-07-24 17:07:14 +0430

    data from weatherstack api has been fetched successfully :)

--- src/controller/city.controller.ts
+++ src/controller/city.controller.ts
@@ -8,14 +8,6 @@ const fetchCityData = async (req: Request, res: Response, next: NextFunction) =>
     const cityName: string = req.params.cityName
     const key = process.env.KEY
     let finalTargetUrl: string = `http://api.weatherstack.com/current?access_key=${key}&query=${cityName}`
-    // needle('get', finalTargetUrl)
-    //     .then(resp => {
-    //         console.log(resp.body);
-    //         // console.log(resp.body.toJSON())
-    //     })
-    //     .catch(err => {
-    //         console.log(err);
-    //     })
     try{
         const resp = await needle('get', finalTargetUrl)
         console.log(resp.body)
